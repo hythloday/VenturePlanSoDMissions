6751841a31602818931721fc9cfcc188e4d0b5aa
Xin Ye <yexinwhu@gmail.com>
2021-08-27 08:44:34 -0500

    fix for mission Customary Shakedown

--- extra-vs-spells.lua
+++ extra-vs-spells.lua
@@ -51,8 +51,8 @@ T.MoreSpells = {
     [327]={type="aura", target="all-other-allies", duration=3, plusDamageDealtATK=20}, --Inspirational Teachings
     [328]={type="nuke", target=0, damageATK=30}, --Applied Lesson
     [329]={type="aura", target=4, modDamageTaken=-50, duration=3}, --Muscle Up
-    [330]={type="aura", target="all-allies", duration=2, plusDamageDealtATK=20}, --Oversight
-    [331]={type="aura", target="all-other-allies", duration=3, plusDamageDealtATK=20}, --Supporting Fire
+    [330]={type="aura", target="all-allies", duration=2, plusDamageDealtATK=24}, --Oversight
+    [331]={type="aura", target="all-other-allies", duration=3, plusDamageDealtATK=24}, --Supporting Fire
     [332]={type="nuke", target=1, damageATK=150}, --Emptied Mug
     [333]={type="aura", target=4, duration=3, modDamageDealt=40}, --Overload
     [334]={type="nuke", target=0, damageATK=90}, --Hefty Package
